b028b793a53f76d88776ef45a32757b8ace219ee
Danish968 <qureshidanish3000@gmail.com>
2022-06-09 18:54:48 +0530

    working on the examples,polymorphism complited

---
+++ CoreJavaProject/src/com/velociter/chapter6/TryPolymorphism.java
@@ -0,0 +1,16 @@
+package com.velociter.chapter6;
+import java.util.Random;
+public class TryPolymorphism {
+
+	public static void main(String[] args) {
+		Animal[] theAnimals = {new Dog("Rover","Poodle"),new Cat("Max","Abyssinian"),new Duck("Daffy","Aylesbury")};
+		Animal petChoice;
+		Random random=new Random();
+		for(int i=0;i<5;i++)
+		{
+			petChoice=theAnimals[(random.nextInt(theAnimals.length))];		}
+		System.out.println("\nyour choice"+"pet");
+		//petChoice.sound(); 
+	}
+
+}
