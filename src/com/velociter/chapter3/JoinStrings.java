362c6c207b036985d40d5900e7bbcc4ce583436b
Shafique98 <mohammed.shafiq.899@gmail.com>
2022-06-20 18:41:39 +0530

    Added file JoinString

---
+++ src/com/velociter/chapter3/JoinStrings.java
@@ -0,0 +1,15 @@
+package com.velociter.chapter3;
+
+public class JoinStrings {
+
+	public static void main(String[] args) {
+	String firstString,secondString,thirdString,joinedString;
+	firstString = "Java ";
+	secondString = "is a ";
+	thirdString= "programming Language";
+	joinedString = firstString+secondString+thirdString;
+	System.out.println(joinedString);
+	
+	}
+
+}
