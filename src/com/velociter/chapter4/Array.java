8c662810059d3b40a9c667a31fa2c99fd2cdb63f
Shafique98 <mohammed.shafiq.899@gmail.com>
2022-06-28 06:52:24 +0530

    Chapter4 Excercize 1,2,4

--- src/com/velociter/chapter4/Array.java
+++ src/com/velociter/chapter4/Array.java
@@ -7,10 +7,11 @@ public class Array {
 		int[] number = new int[101];
 		for(int i=0; i<20; i++) {
 			
-			number[i] = (int) (Math.random()*100-50 );
+			number[i] = (int) (Math.random()*100-50) ;
+			System.out.print(i+1+":"+number[i]+"\n");
 		}
 		for(int i=0; i<20; i++) {
-			System.out.print(i+":"+number[i]+" \n");
+//			System.out.print(i+":"+number[i]+" \n");
 		}
 	
 	}
--- src/com/velociter/chapter4/ReverseThePeragraph.java
+++ src/com/velociter/chapter4/ReverseThePeragraph.java
@@ -6,19 +6,28 @@ from Exercise 3. For instance, “To be or not to be.” would become “oT eb r
 public class ReverseThePeragraph {
 
 	public static void main(String[] args) {
-		String text = "Java is programming";
-		String[] words = text.split(" ");
-		String[] revWords = new String[words.length];
-		StringBuffer sBuffer = new StringBuffer();
+		//String Variable to store peragraph
+		String text = "You can extract a character from a String object by using the charAt() method. This accepts an integer argument that is the offset of the character position from the beginning of the string — in other words";				
+		String[] words = text.split(" ");					//String[] to split text into word.
+		String[] reverseWords = new String[words.length];	//String[] to store reversed words
+		StringBuffer sBuffer = new StringBuffer();			//StringBuffer Object
  
+		for(int i=0; i<words.length; i++) {					//for loop
+			sBuffer.append(words[i]);						//appending word veriable to Sbuffer
+			sBuffer.reverse();								//reversing Sbuffer words
+			reverseWords[i] =sBuffer.toString()+" ";		//storing reverse words in reverseWords[]
+			sBuffer.setLength(0);							//clearing Sbuffer
+			}
+		System.out.println("The peragraph:");
 		for(int i=0; i<words.length; i++) {
-			sBuffer.append(words[i]);
-			sBuffer.reverse();
-			revWords[i] = " "+sBuffer.toString()+" ";
+			System.out.print(words[i]+" ");					//Printing actual peragraph
 		}
-		for(int i=0; i<revWords.length; i++) {
-			System.out.print(revWords[i]);
+		System.out.println("\n\nReversed peragraph:");
+		for(int i=0; i<reverseWords.length; i++) {
+			System.out.print(reverseWords[i]);				//Printing reversed peragraph
 		}
+
+		
 	}
 
 }
