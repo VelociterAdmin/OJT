8c0e24766287c65102c136d2f47f49673c184d6c
Shafique98 <mohammed.shafiq.899@gmail.com>
2022-07-18 18:51:41 +0530

    Exception Files are added today

--- src/com/velociter/chapter6/Experimental.java
+++ src/com/velociter/chapter6/Experimental.java
@@ -2,13 +2,28 @@ package com.velociter.chapter6;
 
 public class Experimental {
 	
-	public static void main(String[] args) {
-	String flag1;
-	flag1 = "true";
-	String flag2 = "tr";
-	int a = 32;
-	System.out.println(flag1.hashCode());
-	System.out.println(flag2.hashCode());
-	System.out.println(a);
-	}
+//    private static void printLength(String str) {
+//        System.out.println(str.length());
+//    }
+
+    public static void main(String args[]) {
+        String myString = null;
+        try {
+        System.out.println(myString.length());
+        System.out.println("Shafique");
+        }
+        catch(NullPointerException e) {
+//			e.printStackTrace();	
+/*
+ * java.lang.NullPointerException: Cannot invoke "String.length()" because "myString" is 
+ * at com.velociter.chapter6.Experimental.main(Experimental.java:12)
+*/
+//        	System.out.println(e.getMessage());
+//        	Cannot invoke "String.length()" because "myString" is null
+
+        	System.out.println(e);
+//		java.lang.NullPointerException: Cannot invoke "String.length()" because "myString" is null
+
+        }
+    }
 }
---
+++ src/com/velociter/chapter7/ExceptionClass.java
@@ -0,0 +1,14 @@
+package com.velociter.chapter7;
+/*
+ * Add an exception class to the last example that will differentiate between the index-out-ofbounds error possibilities,
+ * rethrow an appropriate object of this exception class in divide(), and handle the exception in main().
+*/
+
+public class ExceptionClass extends Exception {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		
+	}
+}
